cf36e17acd04a41b914848c2ee78b65a45dfea99
uumkon <uumkon@gmail.com>
2024-03-03 14:26:44 +0500

    update

--- lab_2/internal/methods/first_to_four.go
+++ lab_2/internal/methods/first_to_four.go
@@ -1,10 +1,14 @@
 package methods
 
-func Row_by_col(matrix_a, matrix_b [][]int32) [][]int32 {
+import "github.com/uumk0n/parallel/lab_2/internal/binaries"
+
+func Row_by_col(bin_a_name, bin_b_name string) [][]int {
+	matrix_a := binaries.ReadBin(bin_a_name)
+	matrix_b := binaries.ReadBin(bin_b_name)
 	n := len(matrix_a)
-	matrix_c := make([][]int32, n)
+	matrix_c := make([][]int, n)
 	for i := range matrix_c {
-		matrix_c[i] = make([]int32, n)
+		matrix_c[i] = make([]int, n)
 	}
 
 	for i := 0; i < n; i++ {
@@ -17,11 +21,13 @@ func Row_by_col(matrix_a, matrix_b [][]int32) [][]int32 {
 	return matrix_c
 }
 
-func Row_by_col_transposed(matrix_a, matrix_b [][]int32) [][]int32 {
+func Row_by_col_transposed(bin_a_name, bin_b_name string) [][]int {
+	matrix_a := binaries.ReadBin(bin_a_name)
+	matrix_b := binaries.ReadBin(bin_b_name)
 	n := len(matrix_a)
-	matrix_c := make([][]int32, n)
+	matrix_c := make([][]int, n)
 	for i := range matrix_c {
-		matrix_c[i] = make([]int32, n)
+		matrix_c[i] = make([]int, n)
 	}
 
 	for i := 0; i < n; i++ {
@@ -34,11 +40,13 @@ func Row_by_col_transposed(matrix_a, matrix_b [][]int32) [][]int32 {
 	return matrix_c
 }
 
-func Row_by_row(matrix_a, matrix_b [][]int32) [][]int32 {
+func Row_by_row(bin_a_name, bin_b_name string) [][]int {
+	matrix_a := binaries.ReadBin(bin_a_name)
+	matrix_b := binaries.ReadBin(bin_b_name)
 	n := len(matrix_a)
-	matrix_c := make([][]int32, n)
+	matrix_c := make([][]int, n)
 	for i := range matrix_c {
-		matrix_c[i] = make([]int32, n)
+		matrix_c[i] = make([]int, n)
 	}
 
 	for i := 0; i < n; i++ {
@@ -51,11 +59,13 @@ func Row_by_row(matrix_a, matrix_b [][]int32) [][]int32 {
 	return matrix_c
 }
 
-func Row_by_row_transposed(matrix_a, matrix_b [][]int32) [][]int32 {
+func Row_by_row_transposed(bin_a_name, bin_b_name string) [][]int {
+	matrix_a := binaries.ReadBin(bin_a_name)
+	matrix_b := binaries.ReadBin(bin_b_name)
 	n := len(matrix_a)
-	matrix_c := make([][]int32, n)
+	matrix_c := make([][]int, n)
 	for i := range matrix_c {
-		matrix_c[i] = make([]int32, n)
+		matrix_c[i] = make([]int, n)
 	}
 
 	for i := 0; i < n; i++ {
